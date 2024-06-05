1c20bdee4a3eca3c89b4738784992538d9dd2d45
cyworld8x <cyworld8x@gmail.com>
2024-06-06 00:10:04 +0700

    add grpc method

--- internal/user/application/grpcserver/rpc_user_service.go
+++ internal/user/application/grpcserver/rpc_user_service.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cyworld8x/go-postgres-kubernetes-grpc/pkg/paseto"
 	"github.com/cyworld8x/go-postgres-kubernetes-grpc/pkg/pb"
 	"github.com/cyworld8x/go-postgres-kubernetes-grpc/pkg/utils"
+	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -34,7 +35,6 @@ func (server *Server) GetLogin(ctx context.Context, req *pb.GetLoginRequest) (*p
 	user, err := server.uc.GetLogin(ctx, req.GetUsername())
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "User name doesn't match: %v", err)
-
 	}
 
 	err = utils.CheckPassword(req.GetPassword(), user.Password)
@@ -43,9 +43,11 @@ func (server *Server) GetLogin(ctx context.Context, req *pb.GetLoginRequest) (*p
 	}
 
 	rsp := &pb.GetLoginResponse{
-		Id:       user.ID.String(),
-		Username: req.GetUsername(),
-		Password: req.GetPassword(),
+		Id:          user.ID.String(),
+		Username:    req.GetUsername(),
+		Email:       user.Email.String,
+		DisplayName: user.DisplayName.String,
+		Role:        user.Role,
 	}
 	maker, _ := paseto.NewPasetoMaker()
 	token, _, err := maker.CreateToken(user.Username, time.Hour)
@@ -55,4 +57,25 @@ func (server *Server) GetLogin(ctx context.Context, req *pb.GetLoginRequest) (*p
 	}
 	rsp.Token = token
 	return rsp, nil
+}
+
+func (server *Server) GetUser(ctx context.Context, req *pb.GetUserIdRequest) (*pb.User, error) {
+	id, err := uuid.Parse(req.GetId())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "User Id is invalid: %v", err)
+	}
+	user, err := server.uc.GetUser(ctx, id)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "User doesn't match: %v", err)
+	}
+	return &pb.User{
+		ID:        user.ID.String(),
+		Username:  user.Username,
+		Email:     user.Email.String,
+		Fullname:  user.DisplayName.String,
+		Password:  user.Password,
+		Role:      user.Role,
+		CreatedAt: timestamppb.New(user.Created),
+		// Add more fields as needed
+	}, nil
 }
